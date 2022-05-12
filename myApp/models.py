8fc9feeb6c41bfa96313d4f904313a8fe0494763
Msan030 <75320855+Msan030@users.noreply.github.com>
2022-05-12 20:53:23 +0800

    database

--- myApp/models.py
+++ myApp/models.py
@@ -6,10 +6,27 @@ from django.db import models
 
 class Records(models.Model):
     rdate = models.DateTimeField()
-    rplace = models.CharField(max_length=20)
-    rnum = models.CharField(max_length=20)
+    rbuilding = models.CharField(max_length=20)
+    rfloor = models.CharField(max_length=20)
     rcat = models.CharField(max_length=20)
     rphoto = models.ImageField(null = True, blank=True)
     isDelete = models.BooleanField(default = False)
-    def __str__(self):
-        return "&s-%d-%d"%(self.rdate,self.rplace,self.rnum,self.rcat,self.rphoto)
+    isHandle = models.BooleanField(default=False)
+    # def __str__(self):
+    #     return "&s-%d-%d"%(self.rdate,self.rbuilding,self.rfloorm,self.rcat,self.rphoto)
+
+    # class Meta:
+    #     # 定义该 model 在数据中的表名称:
+    #     # db_table = 'visitors'
+    #
+    #     db_table = 'project'
+    def to_dict(self):
+        return {
+            'rdate': self.rdate,
+            'rbuilding': self.rbuilding,
+            'rfloor': self.rfloor,
+            'rcat': self.rcat,
+            'rphoto': self.rphoto,
+            'isDelete': self.isDelete,
+            'isHandle': self.isHandle,
+        }
